a6d43229268698a1df9b26c5dfef83809a1f4d63
acostahenry7 <73194577+acostahenry7@users.noreply.github.com>
2024-08-12 11:50:44 -0400

    fix--build

--- client/src/api/logs.js
+++ client/src/api/logs.js
@@ -1,50 +1,50 @@
-const { mainServer } = require("../utils/constants/conections");
-import { addQueryParams } from "../utils/stringFunctions";
+// import { addQueryParams } from "../utils/stringFunctions";
+// const { mainServer } = require("../utils/constants/conections");
 
-async function createTransactionLogApi(params) {
-  try {
-    let data = {
-      name: "App Log",
-      username: params.username,
-      comment: params.comment,
-      objectName: "GA_TRANSACTION_LOGS",
-      appName: "GA_PRICELIST",
-      status: params.status,
-      cookie: params.cookie,
-      target: "U_GA_TRANSACTION_LOGS",
-    };
+// async function createTransactionLogApi(params) {
+//   try {
+//     let data = {
+//       name: "App Log",
+//       username: params.username,
+//       comment: params.comment,
+//       objectName: "GA_TRANSACTION_LOGS",
+//       appName: "GA_PRICELIST",
+//       status: params.status,
+//       cookie: params.cookie,
+//       target: "U_GA_TRANSACTION_LOGS",
+//     };
 
-    const res = await fetch(`${mainServer}/log`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
+//     const res = await fetch(`${mainServer}/log`, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(data),
+//     });
 
-    return res;
-  } catch (error) {
-    throw error;
-  }
-}
+//     return res;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
-async function getTransactionLogsApi(params) {
-  let queryParams = {
-    target: "U_GA_TRANSACTION_LOGS",
-    cookie: params.cookie,
-  };
+// async function getTransactionLogsApi(params) {
+//   let queryParams = {
+//     target: "U_GA_TRANSACTION_LOGS",
+//     cookie: params.cookie,
+//   };
 
-  try {
-    let url = `${mainServer}/log`;
-    url = addQueryParams(url, queryParams);
-    const res = await fetch(url);
-    const result = await res.json();
+//   try {
+//     let url = `${mainServer}/log`;
+//     url = addQueryParams(url, queryParams);
+//     const res = await fetch(url);
+//     const result = await res.json();
 
-    if (result.error) throw new Error(result.body);
+//     if (result.error) throw new Error(result.body);
 
-    return result;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-export { getTransactionLogsApi, createTransactionLogApi };
+// export { getTransactionLogsApi, createTransactionLogApi };
